781b7345c89af3843e7716ae97cea85eb8b90036
Patryk <pat.parkitny@gmail.com>
2022-06-21 16:05:21 +0200

    add gitigrone file, add templates folder and basic html files, edit views.py and add first class

--- pomodoro/timer/views.py
+++ pomodoro/timer/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+
+class MainView(View):
+    """ 
+    This class represent main site of whole app.
+    """
+
+    def get(self, request):
+        return render(request, 'main.html')
+
+    def post(self, request):
+        return
